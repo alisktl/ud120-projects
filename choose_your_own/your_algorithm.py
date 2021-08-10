d9e67445c0c38dea2a62a5ea5b3dcabedc7bae50
alisktl <alisktl@gmail.com>
2021-08-11 01:22:35 +0600

    use AdaBoostClassifier

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
@@ -27,17 +29,20 @@ plt.ylabel("grade")
 plt.show()
 ################################################################################
 
+### Initializing AdaBoostClassifier
+clf = AdaBoostClassifier(n_estimators=50, learning_rate=1.0, random_state=0)
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+### Fitting classifier
+clf.fit(features_train, labels_train)
 
+### Predicting
+pred = clf.predict(features_test)
 
+### Accuracy score
+acc_score = accuracy_score(labels_test, pred)
+print("Accuracy Score", acc_score);
 
-
-
-
-
-
+### Plotting graph
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:

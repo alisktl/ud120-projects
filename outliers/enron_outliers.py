6467c69ab9db95d6359c1127f190bfc94aea617b
alisktl <alisktl@gmail.com>
2021-08-13 21:28:48 +0600

    Identify two more outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -28,4 +28,8 @@ matplotlib.pyplot.ylabel("bonus")
 matplotlib.pyplot.show()
 
 max_bonus_person = [person for person in data_dict if(data_dict[person]["bonus"] == max_bonus)]
-print("Outlier entry:", max_bonus_person)
+#print("Outlier entry:", max_bonus_person)
+
+### Identifying two more outliers
+max_bonus_persons = [person for person in data_dict.keys() if(type(data_dict[person]["bonus"]) == type(5000000)) and (data_dict[person]["bonus"] > 5000000) and (data_dict[person]["salary"] > 1000000)]
+print("Outlier entry:", max_bonus_persons)

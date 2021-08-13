1c73e1cdd4eade1a98071ba9ce844d3b809f351c
alisktl <alisktl@gmail.com>
2021-08-13 21:25:42 +0600

    Remove outlier

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,6 +9,10 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb") )
+
+### Removing outlier
+data_dict.pop("TOTAL", 0)
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 

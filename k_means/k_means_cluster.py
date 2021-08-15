b8e2cd64a5573168a0aadd9a57780952c0f4841a
alisktl <alisktl@gmail.com>
2021-08-15 10:26:55 +0600

    Apply feature scaling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,7 +48,7 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -57,9 +57,15 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
+
+### Applying feature scaling
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(finance_features)
+finance_features = scaler.transform(finance_features)
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
